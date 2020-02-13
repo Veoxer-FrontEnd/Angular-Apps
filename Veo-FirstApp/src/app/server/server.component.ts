595c9ef999c43fd39693e90251ebcab1c251eeb5
Si Mohamed Hadrach <mohamed.hadrach@gmail.com>
2020-02-13 23:45:29 +0100

    tuto n°30

--- Veo-FirstApp/src/app/server/server.component.ts
+++ Veo-FirstApp/src/app/server/server.component.ts
@@ -4,4 +4,28 @@ import {Component} from '@angular/core';
     selector:'app-server',
     templateUrl:'./server.component.html'
 })
-export class ServerComponent{ }
+export class ServerComponent{
+    allowServerIncrement = false;
+    constructor(){
+        setTimeout(() => {
+            this.allowServerIncrement=true;
+        }, 2000);
+    }
+    serverCreation = "No server was created.";
+    serverName = "";
+    serverId:number =   10;
+    serverState:string = 'up';
+
+    getServerState(){
+        return this.serverState;
+    }
+
+    onServerCreate(){
+        this.serverCreation = "Server created !";
+    }
+
+    onSetServerName(event: Event){
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
+
+ }
