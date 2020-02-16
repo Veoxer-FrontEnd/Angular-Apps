5275e350acccd89c9e4613b9205be6f2c0042192
Si Mohamed Hadrach <mohamed.hadrach@gmail.com>
2020-02-16 13:10:47 +0100

    Recipe Book App

---
+++ Recipe-Store/src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe{
+    public Name: string;
+    public Description: string;
+    public ImagePath: string;
+
+    constructor(name: string, description: string, imagePath: string){
+        this.Name = name;
+        this.Description = description;
+        this.ImagePath = imagePath;
+    }
+}
---
+++ Recipe-Store/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Just testing stuff', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Test Recipe', 'Just testing stuff', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
